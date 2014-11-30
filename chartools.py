ca24068291cae06d2226d84ebc78162b2caef7ab
nickdab <nickdab@gmail.com>
2014-11-30 12:57:33 -0700

    Not sure what these commits are, actually.

---
+++ chartools.py
@@ -0,0 +1,8 @@
+# This is a library I'm making to help people create characters and have them interact with the world
+
+class nonPlayable:
+	race = ""
+	age = 0
+	description = ""
+	health = 0
+	hostile = False
--- maptools.py
+++ maptools.py
@@ -1,6 +1,6 @@
 # These are some tools I am creating to help me build the map for the game. They are a work in progress.
 # I am also learning python, so I apologize in advance if I do dumb things.
- 
+
 # Right now this consists of just two classes: locations and coordinate systems.
 # As I have it implemented here, a coordinate system is simply a group of locations. 
 # I'm sure there is more I could do, for instance linking a coordinate system to a location so that
@@ -12,45 +12,44 @@ import array
 import os
 import sys
 
+
 class location:
-	farDesc="" #For Descriptions of locations from far away
-	nearDesc="" #For descriptions of locations from close-up
-	name=""
-	items=[] #I am investigating this, but apparently you can't use the "append" function with items and characters when there is an array of locations, i.e. a coordSystem locationArray. Don't know why this is.
-	characters=[]		
-	isCoordSys=False
+    farDesc = ""  # For Descriptions of locations from far away
+    nearDesc = ""  # For descriptions of locations from close-up
+    name = ""
+    items = []  # I am investigating this, but apparently you can't use the "append" function with items and characters
+                # when there is an array of locations, i.e. a coordSystem locationArray. Don't know why this is.
+    characters = []
+    isCoordSys = False
 
-	def __init__(self):
-		self.farDesc = ""
-		self.nearDesc = ""
-		self.name = ""
-		self.items = []
-		self.characters = []
-		self.isCoordSys = False #save time so that we don't have to check for a matching coordSys for every single location
+    def __init__(self):
+        self.farDesc = ""
+        self.nearDesc = ""
+        self.name = ""
+        self.items = []
+        self.characters = []
+        self.isCoordSys = False  # save time so that we don't have to check for a matching coordSys for every
+                                 # single location
 
-	def __init__(self, isCoordSys):
-		self.farDesc = ""
-		self.nearDesc = ""
-		self.name = ""
-		self.items = []
-		self.characters = []
-		self.isCoordSys = isCoordSys
 
 class coordSystem:
-	name = ""
-	xMax=0
-	yMax=0
-	zMax=0
-	locationArray=[]
+    name = ""
+    xMax = 0
+    yMax = 0
+    zMax = 0
+    _locationArray = []     # I have it as a "private variable" (as far as they exist in Python) to avoid confusion:
+                            # as the implementation stands, you have to access the array in the order of [z][y][x], but
+                            # most people are used to the opposite ordering. Rather than screw around with naming and
+                            # perhaps add confusion, I want people to only ever access the array by get and set methods.
 
-	def getLocation(self,x,y,z):
-		return self.locationArray[z][y][x]
+    def getLocation(self, x, y, z):
+        return self._locationArray[z][y][x]
 
-	def __init__(self,name,xMax,yMax,zMax):
-		self.name = name
-		self.xMax=xMax
-		self.yMax=yMax
-		self.zMax=zMax
-		self.locationArray=[ [ [location() for x in xrange(xMax)] for x in xrange(yMax) ] for x in xrange(zMax) ]
-		#The above line is weird, but I can't think of a better way to initialize an array of arrays
+    def __init__(self, name, xMax, yMax, zMax):
+        self.name = name
+        self.xMax = xMax
+        self.yMax = yMax
+        self.zMax = zMax
+        self._locationArray = [[[location() for x in range(xMax)] for x in range(yMax)] for x in range(zMax)]
 
+    #The above line is weird, but I can't think of a better way to initialize an array of arrays
