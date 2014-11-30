467f3ff9a857a78ff18e351b3f4bc6c5f8c993b2
nickdab <nickdab@gmail.com>
2014-11-30 13:46:52 -0700

    Started working on some more tools
    character and item tools, to be specific

--- chartools.py
+++ chartools.py
@@ -1,8 +1,8 @@
 # This is a library I'm making to help people create characters and have them interact with the world
 
-class nonPlayable:
-	race = ""
-	age = 0
+class character:
+	race 		= ""
+	age			= 0
 	description = ""
-	health = 0
-	hostile = False
+	health 		= 0
+	hostility 	= 0 # The higher the number, the more hostile
---
+++ itemtools.py
@@ -0,0 +1,2 @@
+#This will be a suite of tools to help create an item system
+#for characters and locations.
